891bec94faa48221fd485643459a8d539e83bf1e
orlandofat <orlandofat@gmail.com>
2020-06-25 12:17:54 -0400

    banking lab

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,32 @@
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
+    def initialize(name, balance=1000, status="open")
+        @name = name
+        @balance = balance
+        @status = status
+    end
+
+    def deposit(balance)
+        self.balance += balance
+    end
+
+    def display_balance
+        "Your balance is $#{balance}."
+    end
+    
+    def valid?
+        if self.balance > 0  && self.status == "open"
+            true
+        else 
+            false
+        end
+    end
+    
+    def close_account
+        self.balance = 0
+        self.status = "closed"
+    end
+    
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,50 @@
+require "pry"
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+  def initialize(sender,receiver, status="pending",amount)
+    @sender = sender
+    @receiver = receiver
+    @status = status
+    @amount = amount
+  end
+  
+  def valid?
+      if sender.valid? && receiver.valid?
+        true
+      else
+        false
+      end
+  end
+
+  def execute_transaction
+    if self.status == "pending" && self.receiver.status == "open" && self.sender.status == "open"# && !sender.balance < @amount 
+      puts sender.balance
+      puts @amount
+      puts self.amount
+      puts receiver.balance
+      puts sender.status
+      puts receiver.status
+      if sender.balance >= self.amount
+        sender.balance -= @amount
+        receiver.balance += @amount
+        self.status = "complete"
+      else
+        self.status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      end
+      
+      #sender.balance -= @amount
+      #if sender.balance > 0
+        #return "Transaction rejected. Please check your account balance."
+      #end
+      #receiver.balance += @amount
+      #self.status = "complete"
+    end
+    #self.status = "rejected"
+    #return "Transaction rejected. Please check your account balance."
+    #else rejection message
+  end
+  
+  
 end
