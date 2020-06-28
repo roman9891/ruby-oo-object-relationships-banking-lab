8a9f6139d6960892ba4932cd7724cbca879aea0d
roman9891 <roman9891@gmail.com>
2020-06-28 18:41:33 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,5 @@
 require "pry"
 class Transfer
-  # your code here
   attr_accessor :sender, :receiver, :status, :amount
   def initialize(sender,receiver, status="pending",amount)
     @sender = sender
